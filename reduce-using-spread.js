eda31359861ce8850655ce48e9076d12c4ce7dd0
Jonas Mattsson <jonas.mattsson@verisure.com>
2018-05-11 10:30:10 +0200

    added new example

--- reduce-using-spread.js
+++ reduce-using-spread.js
@@ -1,18 +1,8 @@
 const labelKeys = [
-    'OWA_ACCOUNT_MENU_ITEM',
-    'OWA_ACCOUNT_SUBSCRIPTION_TITLE',
-    'OWA_ACTIVITY_TITLE',
-    'OWA_ARMSTATE_ACTION_CANCEL',
-    'OWA_ARMSTATE_ACTION_OK',
-    'OWA_AUTOMATION_TITLE',
-    'OWA_CLIMATE_GRAPH',
-    'OWA_DEVICE_SETTINGS_TITLE',
-    'OWA_DEVICES_TITLE',
-    'OWA_DOOR_LOCK_ARMING',
-    'OWA_DOOR_LOCK_AUTO_LOCK',
-    'OWA_DOOR_LOCK_CODE',
-    'OWA_DOOR_LOCK_LOCK',
-    'OWA_DOOR_LOCK_PIN_AND_TAG']
+    'FOO',
+    'BAR',
+    'BAZ',
+]
 
     /*
     Dynamically sets key:value using only a defined value, allowing a simple default definition
